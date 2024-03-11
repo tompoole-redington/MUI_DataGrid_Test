90360dd11c1d779146ba1f00e08ee6195b792b49
Tom Poole <tom.poole@redington.co.uk>
2024-03-11 15:41:24 +0000

    Add datagrid

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { DataGridPro, GridFilterModel, GridToolbar, useGridApiRef } from '@mui/x-data-grid-pro';
+import { useDemoData } from '@mui/x-data-grid-generator';
+import { useCallback, useEffect, useState } from 'react';
+import { GridStatePro } from '@mui/x-data-grid-pro/models/gridStatePro';
+
+const VISIBLE_FIELDS = ['name', 'rating', 'country', 'dateCreated', 'isAdmin'];
 
 function App() {
+  const [serverSideFiltering, setServeSideFiltering] = useState<boolean>(false);
+
+  const { data } = useDemoData({
+    dataSet: "Employee",  
+    visibleFields: VISIBLE_FIELDS,
+    rowLength: 100,
+  });
+
+  const apiRef = useGridApiRef();
+
+  const onFilterChange = useCallback((filterModel: GridFilterModel) => {
+    // Here we can save the data you need from the filter model
+    console.log(filterModel);
+  }, []);
+
+  const saveState = () => {
+    const stateJson = JSON.stringify(apiRef.current.exportState());
+    window.localStorage.setItem('gridState', stateJson);
+  };
+
+  const restoreState = () => {
+    const stateJson = window.localStorage.getItem('gridState');
+    if (stateJson === null) return;
+
+    const parsedState = JSON.parse(stateJson as string) as GridStatePro;
+    apiRef.current.restoreState(parsedState);
+  };
+  
+  useEffect(() => {
+    // restore state on load
+    // restoreState();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>DataGrid Demo</h3>
+
+      <p>
+        <label>
+          <input type="checkbox" checked={serverSideFiltering} onChange={() => setServeSideFiltering(!serverSideFiltering)} />
+          Server-side filtering enabled
+        </label>
+      </p>
+
+      <p>
+        <button onClick={saveState}>Save Grid State</button>
+        <button onClick={restoreState}>Restore Grid State</button>
+      </p>
+
+      <DataGridPro {...data}
+      slots={{toolbar: GridToolbar}}
+      filterMode={serverSideFiltering ? 'server' : 'client'}
+      onFilterModelChange={onFilterChange}
+      apiRef={apiRef}
+      slotProps={{
+        filterPanel: {
+          columnsSort: 'asc',
+          filterFormProps: {
+            columnInputProps: {
+            }
+          }
+        }
+      }} />
     </div>
   );
 }
